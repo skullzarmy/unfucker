5b16fe30e81d39017f888dfc1f0992c1f6fb08e7
skllzrmy.tez <joe@poundfit.com>
2023-11-01 18:42:53 -0700

    📦 chore(setup.py): add dependencies and extras_require to improve project setup and compatibility
    🔧 fix(setup.py): update attrs, chardet, exceptiongroup, iniconfig, packaging, pluggy, pytest, tomli, and python-magic versions to ensure compatibility and stability
    
    🔧 fix(setup.py): add python-magic-bin as an extra requirement for the 'bin' extra to provide binary dependencies for python-magic library

--- setup.py
+++ setup.py
@@ -4,7 +4,20 @@ setup(
     name='Unfucker',
     version='0.2',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        'attrs==23.1.0',
+        'chardet==5.2.0',
+        'exceptiongroup==1.1.3',
+        'iniconfig==2.0.0',
+        'packaging==23.2',
+        'pluggy==1.3.0',
+        'pytest==7.4.2',
+        'tomli==2.0.1',
+        'python-magic==0.4.27',
+    ],
+    extras_require={
+        'bin': ['python-magic-bin==0.4.14'],
+    },
     entry_points={
         'console_scripts': [
             'unfucker=unfucker.unfucker:unfuck_entry',
